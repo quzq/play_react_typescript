f36f58190d53475710a6481e01e8e23cf563d647
quzq <quzyam@gmail.com>
2020-01-17 17:03:09 +0900

    add container

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TextContainer from './TextContainer'
+import TextInput from './TextInput'
 interface ItemList {
   id: number
   text: string
 }
 // inputFormに文字を入力しているとき　　（TextInput(子)に渡したい）
 const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  alert(e)
+  alert(e.target.value)
 }
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <TextContainer text="hello" handleInputChange={handleInputChange}></TextContainer>
-      </header>
-    </div>
+       <TextInput text="hello" handleInputChange={handleInputChange}></TextInput>
+   </div>
   );
 }
 
